e623b2afb42288b71a4cfeedf4a8cabf9f4ac234
lcolat <loic.colat@gmail.com>
2014-06-29 17:24:27 +0200

    fct de modification des caracteristique des creature

---
+++ fct_simple/creature_carac_modif.py
@@ -0,0 +1,8 @@
+#fct modification caract creature
+
+def creature_carac_modif(creature,vie,degat):
+
+    creature.vie=creature.vie+vie
+    creature.degat=creature.degat+degat
+
+    return creature

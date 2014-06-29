8d21b59e267e72827dfd847c1ff7be2e4bec26b9
Gaël <gael@MacBook-Pro-de-Backup.local>
2014-06-29 14:04:51 +0200

    test

---
+++ testGael.py
@@ -0,0 +1,14 @@
+import pygame
+import sqlite3
+
+import carte.py
+
+
+"""
+#pygame.locals import *
+from pygame.locals import *
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((640, 480))
+"""

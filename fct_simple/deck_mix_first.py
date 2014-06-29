3f4efe92f0b8b9b8a55e9418964c63ab870d4be6
lcolat <loic.colat@gmail.com>
2014-06-29 15:30:18 +0200

    modification deck_mix_first

--- fct_simple/deck_mix_first.py
+++ fct_simple/deck_mix_first.py
@@ -8,26 +8,31 @@ def deck_mix_first(deck):
     deck_melanger=[]
     r=0
     i=0
-    cpt=len(deck)
+    cpt=len(deck)#compteur de cartes dans le deck
+    
     for i in range(cpt):
-#carte type monstre: 
-        if deck[i]==1 or deck[i]==2 :#carte type sort
+
+        if deck[i]==1 or deck[i]==2 :#carte type monstre et sort (provisoir en attente de la méthode d'identification des 
             deck_inter_monstre_sort.append(deck[i])#deplacement de la carte
         elif deck[i]==3 :#carte de type terrain
             deck_inter_terrain.append(deck[i])
-    cpt_t=len(deck_inter_terrain)
-    cpt_m_s=len(deck_inter_monstre_sort)
-    rep_ct_t=cpt_m_s//cpt_t
+            
+    cpt_t=len(deck_inter_terrain)#compteur carte terrain
+    cpt_m_s=len(deck_inter_monstre_sort)#compteur carte monstre et sort
+    rep_ct_t=cpt_m_s//cpt_t#calcule de l'alternance avec les carte terrrain
     i=0
+    
     while (len(deck_inter_terrain)>0)and(len(deck_inter_monstre_sort)>0):
-        for i in range(rep_ct_t):
+        
+        for i in range(rep_ct_t): #deplacement des carte monstre et terrain dans le deck
            r=random.randint(0,len(deck_inter_monstre_sort)-1)
            deck_melanger.append(deck_inter_monstre_sort[r])
            del (deck_inter_monstre_sort[r])
 
+        #deplacement des cartes terrain dans le deck
         r=random.randint(0,len(deck_inter_terrain)-1)
         deck_melanger.append(deck_inter_terrain[r])
 
     return deck_melanger
 
-print(deck_mix_first(deck=[1,2,3,1,2,3,1,2,3,1,2,3]))
+#print(deck_mix_first(deck=[1,2,3,1,2,3,1,2,3,1,2,3]))#test fctionnemnt deck_mix_first

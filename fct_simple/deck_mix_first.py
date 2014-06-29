f68aca74c4db7f10fc4b1fe448e1743d27f85749
lmerabli <lmerabli@gmail.com>
2014-06-29 14:24:56 +0200

    modification mix_deck et deck_mix_first

--- fct_simple/deck_mix_first.py
+++ fct_simple/deck_mix_first.py
@@ -1,3 +1,21 @@
+#melange orgainse du deck pour le debut de partie
+import random
+
 def deck_mix_first(deck):
 	
-	
+    deck_inter_monstre_sort=[]#deck intermediaire pour stocker les cartes monstre et sort
+    deck_inter_terrain=[]#deck intermediaire pour stocker les cartes terrain
+    deck_melanger=[]
+    r=0
+    i=0
+    cpt=len(deck)
+    for i in range(cpt):
+
+        if deck[i]=='''carte type monstre:'''||deck[i]=='''carte type sort''':
+            deck_inter_monstre_sort.append(deck[i])#deplacement de la carte
+        elif deck[i]=='''carte de type terrain''':
+                deck_inter_terrain.append(deck[i])
+    
+        
+                    
+                    
--- fct_simple/mix_deck.py
+++ fct_simple/mix_deck.py
@@ -3,16 +3,16 @@ import random
 
 def deck_mix(deck):
 	
-	deck_inter=[]#definition deck intermediaire 
+	deck_melanger=[]#definition deck melanger 
 	r=0
 	i=0
 	cpt=len(deck)
-	#transfert de carte aleatoirement dans le deck intermediaire
+	#transfert de carte aleatoirement dans le deck melanger
 	for  i in range(cpt):
 		
 		r=random.randint(0,len(deck)-1)#nb aleatoire entre 0 et la derniere case du deck de depart 
-		deck_inter.append(deck[r])#deplacement de la carte dans le deck intermediaire
+		deck_melanger.append(deck[r])#deplacement de la carte dans le deck intermediaire
 		del (deck[r])#supression de la carte transferee
-	return deck_inter
+	return deck_melanger
 
 #print(deck_mix(deck=[1,2,3,4,5,6]))#test fonctionnement fct deck_mix

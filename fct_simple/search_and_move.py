6acbc68f09f087d670e60c2aafd2e17ce9f71ece
lcolat <loic.colat@gmail.com>
2014-06-29 16:50:52 +0200

    nouvelle fct search and move

---
+++ fct_simple/search_and_move.py
@@ -0,0 +1,16 @@
+#fction de recherche de carte et deplacement
+
+def search_and_move(deck,card_ID,position):
+
+    cpt=len(deck)
+    i=0
+    
+    while deck[i].idCarte!=card_ID :#recherche de la carte dans le deck
+        i=i+1
+
+    deck.insert(position-1,deck[i])
+    del(deck[i])
+
+    return deck
+
+    

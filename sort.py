53f1b569736e63e0e64f5b46d4605cd9e080cd73
FlorianDeParis <florian.tournay@hotmail.fr>
2014-07-01 00:22:12 +0200

    ...

--- sort.py
+++ sort.py
@@ -1,12 +1,77 @@
 class sort(carte): #declaration class
     
 
-    def __init__(self):#constructeur
-        self.idSort = idSort
-        self.type=type
-        self.cibleSort = {  'cible'     =  0
-                            ,'creature' =  0
-                            ,'joueur'   =  0
-                            }
- # ce tableau va permettre de savoir ce que peut cibler le sortilege, et donc de limiter dans le graphisme les débordements
+  def __init__(self):#constructeur
+      self.idSort = idSort
+      self.type=type
+      self.cibleSort = {  'cible'     =  0
+                          ,'creature' =  0
+                          ,'joueur'   =  0
+                          }
+  def Renfort_1():
+    # Coût dexploitation: 1 générique + 1 blanc
+    # Description: défaussez-vous de cette carte: Mettez un marqueur +1/+1 sur la créature ciblée
+
+  def Vol():
+    # Quand la créature arrive sur le champ de bataille, vous pouvez chercher dans votre bibliothèque ... 
+    # ... jusqu'à trois cartes, les révéler, les mettre dans votre main, puis mélanger votre bibliothèque.
+
+  def Aptitude_Missionnaire():
+    # Quand le Missionnaire solitaire arrive sur le champ de bataille, vous gagnez 4 points de vie.
+
+  def Toucheterre():
+    # A chaque fois qu'un terrain arrive sur le champ de bataille sous votre contrôle, ...
+    # ... la créature gagne +2/+2 jusqu'à la fin du tour.
+
+  def Art_des_metaux():
+    # Le champion gravé a la protection contre toutes les couleurs tant que vous contrôlez au moins trois artefacts.
+
+  def Regeneration():
+    # Coût d'exploitation: 1 vert
+    # Description: regénérez la créature (La prochaine fois que cette créature devrait être détruite ce tour-ci, elle ne l'est pas. ...
+    # ... A la place, engagez-la, retirez-lui toutes ses blessures et retirez-la du combat.)
+
+  def Generation_de_terrain():
+    # Vous pouvez jouer un terrain supplémentaire pendant chacun de vos tours.
+
+  def Illusion():
+    # Jouez avec la carte du dessus de votre bibliothèque révélée.
+
+  def Parole_de_l_oracle():
+    # Vous pouvez jouer la carte du dessus de votre bibliothèque si c'est une carte de terrain.
+
+  def Celerite():
+    # La creature ne peut pas être la cible de sorts non-verts ou de capacités de sources non-vertes.
+
+  def Desillusion_inevitable():
+    # La créature ne peut pas être contrecarée
+
+  def Oeil_pour_oeil():
+    # A chaque fois que la créature inflige des blessures de combat à un joueur, ce joueur met autant de cartes... 
+    # ... du dessus de sa bibliothèque dans son cimetière
+
+  def Reincarnation_magique():
+    # Quand la créature arrive sur le champ de bataille, renvoyer une carte d'éphémère ou de rituel ciblée ...
+    # ... depuis votre cimetière dans votre main.
+
+  def Destin_maccabre():
+    # La créature gagne +4/+4 tant qu'un adversaire a au moins dix cartes dans son cimetière.
+
+  def Amelioration():
+    # Coût d'exploitation: 1 générique + (flèche retournée?)
+    # Description: Mettez un marqueur +1/+1 sur la créature.
+
+  def Poison_ardent():
+    # Quand la créature arrive sur le champ de bataille, les créatures que vos adversaires contrôlent ...
+    # ... gagnent -2/-2 jusqu'à la fin du tour.
+
+  def Malediction():
+    # A chaque fois qu'une créature qu'un adversaire contrôle est mise dans un cimetière depuis ...
+    # ... le champs de bataille, ce joueur perd 2 points de vie. 
+
+  def Destruction():
+    # Conditions: (flêche retournée?)
+    # Description: Détruisez la créature ciblée. Elle ne peut pas être regénérée.
+  
+# ce tableau va permettre de savoir ce que peut cibler le sortilege, et donc de limiter dans le graphisme les débordements
 # il y aura un switch case pour attribuer un id
